88da7143dd451c451febaabc0e101ff6f9846138
Serikov Bakhyt <serikovbk0@gmail.com>
2024-02-13 07:53:24 +0600

    upd

--- nixos/optionals/flatpak.nix
+++ nixos/optionals/flatpak.nix
@@ -1,7 +1,7 @@
 # Enables flatpak and adds flathub repository
 { pkgs, ... }: {
   services.flatpak.enable = true;
-#  system.activationScripts.flathub.text = ''
- #   ${pkgs.flatpak}/bin/flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-  #'';
+  system.activationScripts.flathub.text = ''
+    ${pkgs.flatpak}/bin/flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+  '';
 }
